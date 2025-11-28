49308cd794ff06ce699e0335f44ba6db0459ce75
kendfss <k3ndfss@gmail.com>
2025-11-28 23:15:34 +0100

    ]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3]9;4;3update: shebang from `#!/usr/bin/env zsh` to `#!/bin/env zsh`; conditional plugin installation logic and syntax for tmux and bash plugins; add: `REPO_HOST` environment variable;]9;4;3]9;4;3]9;4;0

--- deploy-termux.zsh
+++ deploy-termux.zsh
@@ -1,8 +1,9 @@
-#!/usr/bin/env zsh
+#!/bin/env zsh
 export DOTFILES=$HOME/.dotfiles
 export ZDOTDIR="$DOTFILES"
 PATH="$PATH:$DOTFILES/scripts:$HOME/.local/bin:$HOME/go/bin"
 [ -z "$CLONEDIR" ] && export CLONEDIR=$HOME/gitclone/clones
+[ -z "$REPO_HOST" ] && export REPO_HOST=https://github.com
 
 
 [ -z "$DOTFILES" ] && echo "\$DOTFILES is undefined" && exit 1
@@ -39,22 +40,16 @@ if [[ -n "$(command -v pkg)" ]]; then
     [[ ! -e "$ZSH_PLUGINS/$name" ]] && { git clone --depth=1 "https://github.com/zsh-users/$name" "$ZSH_PLUGINS/$name" && source "$ZSH_PLUGINS/$name/$name.zsh"; }
   done
   [[ ! -e "$ZSH_PLUGINS/zsh-sweep" ]] && { git clone https://github.com/psprint/zsh-sweep "$ZSH_PLUGINS/zsh-sweep" && source "$DOTFILES/.zshrc"; }
-  
+
   export TMUX_PLUGINS="$HOME/.tmux/plugins"
   local origin="$(pwd)"
   mkdir -p "$TMUX_PLUGINS"
-  if git clone https://github.com/tmux-plugins/tpm "$TMUX_PLUGINS"; then
-    symlinkDialogue "$(pwd)" "$TMUX_PLUGINS/tpm"
-    cd "$origin"
-  else
-    echo "couldn't clone tmux-plugins/tpm"
-    cd "$origin"
-    exit 1
-  fi
+  { [ ! -e "$CLONEDIR/tmux-plugins/tpm" ] && { clone tmux-plugins/tpm || { echo "couldn't clone tmux-plugins/tpm"; exit 1; }; }; } || cd "$CLONEDIR/tmux-plugins/tpm"
+  { symlinkDialogue "$(pwd)" "$TMUX_PLUGINS/tpm" && cd "$origin"; } || exit 1
 
   export BASH_PLUGINS="$DOTFILES/bash-plugins"
   mkdir -p "$BASH_PLUGINS"
-  [[ ! -e "$BASH_PLUGINS/ble.sh" ]] && { git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git "$BASH_PLUGINS/ble.sh" && make -C "$BASH_PLUGINS/ble.sh" && source "$BASH_PLUGINS/ble.sh/out/ble.sh"; }
+  { [[ ! -e "$BASH_PLUGINS/ble.sh" ]] && { git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git "$BASH_PLUGINS/ble.sh" && make -C "$BASH_PLUGINS/ble.sh" && source "$BASH_PLUGINS/ble.sh/out/ble.sh"; }; } || echo "couldn't find, or clone, akinomyoga/ble.sh" && exit 1
 
 else
   echo "pkg is not available. Make sure you have the package manager for termux installed." && return 1
